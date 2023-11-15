6d60f0c25b826e28888fdbe25e206050b8b9c9b2
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2023-11-15 19:01:29 -0300

    Create q7.py

---
+++ Lista1/q7.py
@@ -0,0 +1,157 @@
+func1 = input()
+pfunc1 = int(input())
+func2 = input()
+pfunc2 = int(input())
+func3 = input()
+pfunc3 = int(input())
+func4 = input()
+pfunc4 = int(input())
+func5 = input()
+pfunc5 = int(input())
+
+if (func1 == "novo lançador de teias" and not func2 == "NADA"):
+    print("Com calma, aranha")
+
+if (func1 == "novo lançador de teias" and func2 == "lentes de visão avançada"):
+    print("Lembre de desativar as lentes depois, e cuidado ao usar as teias no escuro")
+
+if (func1 == "novo lançador de teias" and func2 == "lentes de visão avançada" and func3 == "traje à prova de balas"):
+    print("UOU, só tente sair dessa vivo, vou otimizar a energia aqui")
+
+if (func1 == "ativação de inteligência artificial" or func2 == "ativação de inteligência artificial" or func3 == "ativação de inteligência artificial" or func4 == "ativação de inteligência artificial" or func5 == "ativação de inteligência artificial"):
+    print("Espero que não esteja ativando isso para usar nas provas")
+    
+if (pfunc1 + pfunc2 + pfunc3 + pfunc4 + pfunc5 >= 80):
+    print("Vou descarregar em questão de minutos, pare AGORA")
+
+if (func1 == "membranas planadoras" and func2 == "novo lançador de teias" and func3 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "membranas planadoras" and func2 == "ativação de inteligência artificial" and func3 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "novo lançador de teias" and func2 == "membranas planadoras" and func3 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "novo lançador de teias" and func2 == "ativação de inteligência artificial" and func3 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "ativação de inteligência artificial" and func2 == "membranas planadoras" and func3 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "ativação de inteligência artificial" and func2 == "novo lançador de teias" and func3 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+
+elif (func2 == "membranas planadoras" and func3 == "novo lançador de teias" and func4 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func2 == "membranas planadoras" and func3 == "ativação de inteligência artificial" and func4 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func2 == "novo lançador de teias" and func3 == "membranas planadoras" and func4 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func2 == "novo lançador de teias" and func3 == "ativação de inteligência artificial" and func4 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func2 == "ativação de inteligência artificial" and func3 == "membranas planadoras" and func4 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func2 == "ativação de inteligência artificial" and func3 == "novo lançador de teias" and func4 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+
+elif (func3 == "membranas planadoras" and func4 == "novo lançador de teias" and func5 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func3 == "membranas planadoras" and func4 == "ativação de inteligência artificial" and func5 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func3 == "novo lançador de teias" and func4 == "membranas planadoras" and func5 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func3 == "novo lançador de teias" and func4 == "ativação de inteligência artificial" and func5 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func3 == "ativação de inteligência artificial" and func4 == "membranas planadoras" and func5 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func3 == "ativação de inteligência artificial" and func4 == "novo lançador de teias" and func5 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+
+elif (func1 == "membranas planadoras" and func2 == "novo lançador de teias" and func4 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "membranas planadoras" and func2 == "ativação de inteligência artificial" and func4 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "novo lançador de teias" and func2 == "membranas planadoras" and func4 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "novo lançador de teias" and func2 == "ativação de inteligência artificial" and func4 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "ativação de inteligência artificial" and func2 == "membranas planadoras" and func4 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "ativação de inteligência artificial" and func2 == "novo lançador de teias" and func4 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+
+elif (func1 == "membranas planadoras" and func2 == "novo lançador de teias" and func5 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "membranas planadoras" and func2 == "ativação de inteligência artificial" and func5 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "novo lançador de teias" and func2 == "membranas planadoras" and func5 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "novo lançador de teias" and func2 == "ativação de inteligência artificial" and func5 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "ativação de inteligência artificial" and func2 == "membranas planadoras" and func5 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "ativação de inteligência artificial" and func2 == "novo lançador de teias" and func5 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+
+elif (func1 == "membranas planadoras" and func3 == "novo lançador de teias" and func4 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "membranas planadoras" and func3 == "ativação de inteligência artificial" and func4 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "novo lançador de teias" and func3 == "membranas planadoras" and func4 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "novo lançador de teias" and func3 == "ativação de inteligência artificial" and func4 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "ativação de inteligência artificial" and func3 == "membranas planadoras" and func4 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "ativação de inteligência artificial" and func3 == "novo lançador de teias" and func4 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+
+elif (func1 == "membranas planadoras" and func3 == "novo lançador de teias" and func5 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "membranas planadoras" and func3 == "ativação de inteligência artificial" and func5 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "novo lançador de teias" and func3 == "membranas planadoras" and func5 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "novo lançador de teias" and func3 == "ativação de inteligência artificial" and func5 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "ativação de inteligência artificial" and func3 == "membranas planadoras" and func5 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "ativação de inteligência artificial" and func3 == "novo lançador de teias" and func5 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+
+elif (func1 == "membranas planadoras" and func4 == "novo lançador de teias" and func5 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "membranas planadoras" and func4 == "ativação de inteligência artificial" and func5 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "novo lançador de teias" and func4 == "membranas planadoras" and func5 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "novo lançador de teias" and func4 == "ativação de inteligência artificial" and func5 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "ativação de inteligência artificial" and func4 == "membranas planadoras" and func5 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func1 == "ativação de inteligência artificial" and func4 == "novo lançador de teias" and func5 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa") 
+
+elif (func2 == "membranas planadoras" and func3 == "novo lançador de teias" and func5 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func2 == "membranas planadoras" and func3 == "ativação de inteligência artificial" and func5 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func2 == "novo lançador de teias" and func3 == "membranas planadoras" and func5 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func2 == "novo lançador de teias" and func3 == "ativação de inteligência artificial" and func5 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func2 == "ativação de inteligência artificial" and func3 == "membranas planadoras" and func5 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func2 == "ativação de inteligência artificial" and func3 == "novo lançador de teias" and func5 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+
+elif (func2 == "membranas planadoras" and func4 == "novo lançador de teias" and func5 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func2 == "membranas planadoras" and func4 == "ativação de inteligência artificial" and func5 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func2 == "novo lançador de teias" and func4 == "membranas planadoras" and func5 == "ativação de inteligência artificial"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func2 == "novo lançador de teias" and func4 == "ativação de inteligência artificial" and func5 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func2 == "ativação de inteligência artificial" and func4 == "membranas planadoras" and func5 == "novo lançador de teias"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa")
+elif (func2 == "ativação de inteligência artificial" and func4 == "novo lançador de teias" and func5 == "membranas planadoras"):
+    print("Tudo certo, mas cuidado ao ficar conversando com IA enquanto voa") 
+
+print(f"Aranha, nessa sequência você usou {pfunc1 + pfunc2 + pfunc3 + pfunc4 + pfunc5} de energia!")
