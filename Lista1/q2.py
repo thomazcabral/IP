e70a197dbabdb0f2e74f6b0d85a2343544fa4dd5
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2023-11-15 18:59:30 -0300

    Create q2.py

---
+++ Lista1/q2.py
@@ -0,0 +1,13 @@
+local = input()
+ocorrencia = input()
+
+if (local == "Queens" or local == "Times square" or local == "Central Park"):
+    print("Parece que hoje teremos que dar umas teiada por aí... simboora!")
+    if (local == "Queens"):
+        print(f"Eita... {ocorrencia}, e logo aqui perto da tia May viiu?!")
+    elif (local == "Times square"):
+        print(f"Movimentação intensa de cria aqui pra variar, mas tem {ocorrencia}, não posso deixar isso acontecer!")
+    else:
+        print(f"Nossa... {ocorrencia}, depois de salvar vou pegar um hot dog meermo.")
+else:
+    print("Tudo maravilindo! mas tenho que estudar para prova de Inglês e História ainda boy...")
