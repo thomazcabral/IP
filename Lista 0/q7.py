ad19d0a705cf22c5005d5d2881d973da4a66ecdf
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2023-11-15 18:57:59 -0300

    Create q7.py

---
+++ Lista 0/q7.py
@@ -0,0 +1,10 @@
+precisa = int(input(""))
+
+if (0 < precisa < 11):
+    print("Arthur")
+elif (10 < precisa < 31):
+    print("Luiz")
+elif (30 < precisa < 101):
+    print("Pedro")
+else:
+    print("Nenhum")
